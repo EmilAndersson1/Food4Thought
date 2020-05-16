ea950e2eeaa67d6be3ac5117459256148d08a938
SamuelAhlberg <samuel_ahlberg@hotmail.com>
2020-05-16 16:29:07 +0200

    fixat ingrediens formulär

--- food4thought_app/static/script.js
+++ food4thought_app/static/script.js
@@ -1,6 +1,7 @@
 var counter = 1;
 var limit = 20;
-function addInput(){
+function addInput(event){
+     event.preventDefault();
      if (counter == limit)  {
           alert("You have reached the limit of adding " + counter + " inputs");
      }
@@ -14,6 +15,24 @@ function addInput(){
         + "<div class='col-md-1'>" + "<label for='measurement'>" +" Mått: " + "</label>" + "<input type='text' id='measurement' class='mt-2 mr-1 form-control' name='measurement" + (counter) + "' required>" + "</div>";
         document.getElementById("dynamicInput").appendChild(newdiv);
      }
+     return false;
 }
-
-$("#add_ingredient").click(addInput);
+$(document).ready(function(){
+     $("#add_ingredient").off('click').on('click', function(){
+          if (counter == limit)  {
+               alert("You have reached the limit of adding " + counter + " inputs");
+          }
+          else {
+             counter++;
+             $("#dynamicInput").append(
+               "<div class='form-row'>" +
+               "<div class='col-md-3'>" + "<label for='ingredient_name'>" +  "Ingrediens " + (counter) + "</label>" + ": "
+               + "<input type='text' id='ingredient_name' class='mt-2 mr-1 form-control' name='ingredient_name" + (counter) + "' required>" + "</div>" 
+               + "<div class='col-md-1'>" +  "<label for='volume'>" +" Mängd: " + "</label>" + "<input type='text' id='volume' class='mt-2 mr-1 form-control' name='volume" + (counter) + "' required>" + "</div>"
+               + "<div class='col-md-1'>" + "<label for='measurement'>" +" Mått: " + "</label>" + "<input type='text' id='measurement' class='mt-2 mr-1 form-control' name='measurement" + (counter) + "' required>" + "</div>"
+               + "</div>"
+             );
+          }
+          return false;
+     });
+})
