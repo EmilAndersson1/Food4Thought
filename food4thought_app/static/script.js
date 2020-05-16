36ab95fdb741c3e371e6b7ac57dfd1cf16d4cf06
SamuelAhlberg <samuel_ahlberg@hotmail.com>
2020-05-16 16:33:57 +0200

    Update script.js

--- food4thought_app/static/script.js
+++ food4thought_app/static/script.js
@@ -1,22 +1,6 @@
 var counter = 1;
 var limit = 20;
-function addInput(event){
-     event.preventDefault();
-     if (counter == limit)  {
-          alert("You have reached the limit of adding " + counter + " inputs");
-     }
-     else {
-        counter++;
-        var newdiv = document.createElement('div');
-        newdiv.setAttribute('class','form-row');
-        newdiv.innerHTML = "<div class='col-md-3'>" + "<label for='ingredient_name'>" +  "Ingrediens " + (counter) + "</label>" + ": "
-        + "<input type='text' id='ingredient_name' class='mt-2 mr-1 form-control' name='ingredient_name" + (counter) + "' required>" + "</div>" 
-        + "<div class='col-md-1'>" +  "<label for='volume'>" +" Mängd: " + "</label>" + "<input type='text' id='volume' class='mt-2 mr-1 form-control' name='volume" + (counter) + "' required>" + "</div>"
-        + "<div class='col-md-1'>" + "<label for='measurement'>" +" Mått: " + "</label>" + "<input type='text' id='measurement' class='mt-2 mr-1 form-control' name='measurement" + (counter) + "' required>" + "</div>";
-        document.getElementById("dynamicInput").appendChild(newdiv);
-     }
-     return false;
-}
+
 $(document).ready(function(){
      $("#add_ingredient").off('click').on('click', function(){
           if (counter == limit)  {
