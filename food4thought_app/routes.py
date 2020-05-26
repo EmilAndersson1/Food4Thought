8f574c00ec71b05ede0ffb3f8c7fd1d7e2957df6
Emil Andersson <e.andersson22@gmail.com>
2020-05-26 12:54:47 +0200

    lagt till lite frontend på profilsidan

--- food4thought_app/routes.py
+++ food4thought_app/routes.py
@@ -2,8 +2,9 @@ from flask import render_template, url_for, request, redirect, session, flash
 from food4thought_app import app
 from food4thought_app.database import db
 from datetime import datetime
-
+import os
 from random import randint
+from werkzeug.utils import secure_filename
 
 @app.route('/')
 def index():
@@ -205,3 +206,30 @@ def add_comment():
 
     return redirect("/recipe/{}/".format(recipe_id))
 
+UPLOAD_FOLDER = "/static/images/"
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_profile_image/', methods=['POST'])
+def upload_profile_image():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template(url_for("profile"))
