26d30b0812cadf927bba272ead3cd3cf39184e08
Emil Andersson <e.andersson22@gmail.com>
2020-05-27 14:23:23 +0200

    lagt till js för att få upp namn på fil som man valt..

--- food4thought_app/static/script.js
+++ food4thought_app/static/script.js
@@ -20,3 +20,8 @@ $(document).ready(function(){
           return false;
      });
 })
+
+$('input[type="file"]').change(function(e){
+     var fileName = e.target.files[0].name;
+     $('.custom-file-label').html(fileName);
+ });
